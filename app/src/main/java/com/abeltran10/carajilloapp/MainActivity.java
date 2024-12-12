071f7ecf484a960ad8b8152e13b5f7fd71aa96cf
Your Name <you@example.com>
2024-12-12 17:39:23 +0100

    afegida toolbar

--- app/src/main/java/com/abeltran10/carajilloapp/MainActivity.java
+++ app/src/main/java/com/abeltran10/carajilloapp/MainActivity.java
@@ -1,24 +1,27 @@
 package com.abeltran10.carajilloapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.abeltran10.carajilloapp.ui.login.LoginFragment;
 
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("UseSupportActionBar")
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-        Objects.requireNonNull(getSupportActionBar()).setHomeAsUpIndicator(R.mipmap.ic_launcher_round);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher_round);
 
         if (savedInstanceState == null)
             getSupportFragmentManager().beginTransaction().setReorderingAllowed(true)
