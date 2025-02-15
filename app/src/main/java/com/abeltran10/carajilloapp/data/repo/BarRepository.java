894371b6ec7e8b44c3be2fff11710af10fe185fb
abeltran10 <you@example.com>
2025-02-15 19:46:37 +0100

    Creat LocationService per validar l'adreça

--- app/src/main/java/com/abeltran10/carajilloapp/data/repo/BarRepository.java
+++ app/src/main/java/com/abeltran10/carajilloapp/data/repo/BarRepository.java
@@ -3,6 +3,8 @@ package com.abeltran10.carajilloapp.data.repo;
 import com.abeltran10.carajilloapp.data.RepositoryCallback;
 import com.abeltran10.carajilloapp.data.Result;
 import com.abeltran10.carajilloapp.data.model.Bar;
+import com.abeltran10.carajilloapp.data.service.LocationServiceImpl;
+import com.abeltran10.carajilloapp.data.service.LocationService;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.CollectionReference;
@@ -12,9 +14,12 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+
 
 public class BarRepository {
 
@@ -47,10 +52,11 @@ public class BarRepository {
                 .build();
     }
 
-    public void asyncCreateBar(String name, String address, String city, String postalCode, RepositoryCallback<Bar> callback) {
+    public void asyncCreateBar(String name, String address, String number, String city, String postalCode,
+                               RepositoryCallback<Bar> callback) {
         Runnable runnable = () -> {
             try {
-                Result<Bar> result = createBar(name, address, city, postalCode);
+                Result<Bar> result = createBar(name, address, number, city, postalCode);
                 callback.onComplete(result);
             } catch (Exception e) {
                 Result<Bar> errorResult = new Result.Error(new IOException("Error al afegir el bar a la base de dades"));
@@ -61,12 +67,15 @@ public class BarRepository {
         new Thread(runnable).start();
     }
 
-    private Result<Bar> createBar(String name, String address, String city, String postalCode) {
+    private Result<Bar> createBar(String name, String address, String number, String city, String postalCode) {
+        LocationService location = new LocationServiceImpl();
         Result<Bar> result = null;
 
         name = name.toUpperCase();
         address = address.toUpperCase();
         city = city.toUpperCase();
+
+        boolean isAddresValid = location.isAddressValid(address, number, postalCode, city);
 
         Query q = bd.collection("bars").where(Filter.and(Filter.equalTo("address",address),
                 Filter.equalTo("city", city), Filter.equalTo("postalCode", postalCode)));
@@ -74,10 +83,10 @@ public class BarRepository {
         try {
             QuerySnapshot querySnapshot = Tasks.await(q.get());
 
-            if (querySnapshot.getDocuments().isEmpty()) {
+            if (querySnapshot.getDocuments().isEmpty() && isAddresValid) {
                 Map<String, Object> map = new HashMap<>();
                 map.put("name", name);
-                map.put("address", address);
+                map.put("address", address + " " + number);
                 map.put("city", city);
                 map.put("postalCode", postalCode);
                 map.put("rating", 0.0);
@@ -94,7 +103,7 @@ public class BarRepository {
 
                 result = new Result.Success<Bar>(bar);
             } else {
-                result = new Result.Error(new IOException("Ja s'ha registrat aquest bar"));
+                result = new Result.Error(new IOException("Ja s'ha registrat aquest bar o l'adreça no és valida"));
             }
 
         } catch (ExecutionException | InterruptedException e) {
---
+++ app/src/main/java/com/abeltran10/carajilloapp/data/service/LocationServiceImpl.java
@@ -0,0 +1,49 @@
+package com.abeltran10.carajilloapp.data.service;
+
+import com.abeltran10.carajilloapp.BuildConfig;
+
+import org.json.JSONObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class LocationServiceImpl implements LocationService {
+
+    public static final String APIKEY = BuildConfig.APIKEY;
+    private static final String BASE_URL = "https://api.opencagedata.com/geocode/v1/json";
+
+    @Override
+    public boolean isAddressValid(String address, String number, String postalCode, String city) {
+        OkHttpClient client = new OkHttpClient();
+
+        String formatedAddress = address.replace(" ", "%20") + "+" + number + "%2C+" + postalCode + "+" + city.replace(" ", "%20") + "%2C+" + "Spain";
+
+        try {
+
+            String url = BASE_URL + "?q=" + formatedAddress + "&key=" + APIKEY;
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .get()
+                    .build();
+
+            Response response = client.newCall(request).execute();
+
+            if (response.isSuccessful()) {
+                String responseBody = response.body().string();
+
+                // Parsear la respuesta JSON
+                JSONObject json = new JSONObject(responseBody);
+                int totalResults = json.getInt("total_results");
+
+                // si recupera direcció i códi postal com a mínim
+                return totalResults >= 2;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
